fc2919e1a1cdf5502aa6f4e591c127bc3a41d652
Vitaliy <theyamshikov@yandex.ru>
2020-02-29 20:08:04 +0600

    feat: where construct params

--- src/Domain/Entities/Query/Where.php
+++ src/Domain/Entities/Query/Where.php
@@ -8,9 +8,17 @@ class Where
 {
 
     public $column;
-    public $operator = OperatorEnum::EQUAL;
-    public $value = null;
-    public $boolean = 'and';
-    public $not = false;
+    public $operator;
+    public $value;
+    public $boolean;
+    public $not;
 
+    public function __construct($column = null,  $value = null, $operator = OperatorEnum::EQUAL, $boolean = 'and', $not = false)
+    {
+        $this->column = $column;
+        $this->value = $value;
+        $this->operator = $operator;
+        $this->boolean = $boolean;
+        $this->not = $not;
+    }
 }
