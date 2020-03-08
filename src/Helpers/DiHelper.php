c63ce24a4750076fcf1d54c6ec871ee8d65bc08b
Vitaliy <theyamshikov@yandex.ru>
2020-03-08 21:50:00 +0600

    fix: up container interface

--- src/Helpers/DiHelper.php
+++ src/Helpers/DiHelper.php
@@ -2,15 +2,16 @@
 
 namespace PhpLab\Core\Helpers;
 
+use PhpLab\Core\Libs\Container\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface as SymfonyContainerAwareInterface;
 use Psr\Container\ContainerInterface;
-use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 
 class DiHelper
 {
 
     public static function make(string $className, ContainerInterface $container = null): object {
         $instance = new $className;
-        if($instance instanceof ContainerAwareInterface) {
+        if($instance instanceof ContainerAwareInterface || $instance instanceof SymfonyContainerAwareInterface) {
             $instance->setContainer($container);
         }
         return $instance;
