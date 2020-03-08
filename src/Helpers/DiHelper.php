57d12172d60eba102c855cd19c7d1d80ebb4550e
Vitaliy <theyamshikov@yandex.ru>
2020-03-09 00:31:35 +0600

    refacto

--- src/Helpers/DiHelper.php
+++ src/Helpers/DiHelper.php
@@ -3,18 +3,28 @@
 namespace PhpLab\Core\Helpers;
 
 use PhpLab\Core\Libs\Container\ContainerAwareInterface;
-use Symfony\Component\DependencyInjection\ContainerAwareInterface as SymfonyContainerAwareInterface;
 use Psr\Container\ContainerInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface as SymfonyContainerAwareInterface;
 
 class DiHelper
 {
 
-    public static function make(string $className, ContainerInterface $container = null): object {
-        $instance = new $className;
-        if($instance instanceof ContainerAwareInterface || $instance instanceof SymfonyContainerAwareInterface) {
+    public static function make(string $className, ContainerInterface $container = null): object
+    {
+        if($container->has($className)) {
+            $instance = $container->get($className);
+        } else {
+            $instance = new $className;
+        }
+        self::setContainer($instance, $container);
+        return $instance;
+    }
+
+    public static function setContainer(object $instance, ContainerInterface $container)
+    {
+        if ($instance instanceof ContainerAwareInterface || $instance instanceof SymfonyContainerAwareInterface) {
             $instance->setContainer($container);
         }
-        return $instance;
     }
 
 }
