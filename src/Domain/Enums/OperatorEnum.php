0611cf7b8eb57d98ed4948d20bc57605de58aa59
Vitaliy <theyamshikov@yandex.ru>
2020-03-10 00:09:31 +0600

    feat: make operators

--- src/Domain/Enums/OperatorEnum.php
+++ src/Domain/Enums/OperatorEnum.php
@@ -7,11 +7,15 @@ use PhpLab\Core\Domain\Base\BaseEnum;
 class OperatorEnum extends BaseEnum
 {
 
+    const NULL = 'NULL';
+    const NOT_NULL = 'NOT NULL';
     const EQUAL = '=';
     const NOT_EQUAL = '<>';
     //const NOT_EQUAL = '!=';
     const LESS = '<';
+    const LESS_OR_EQUAL = '<=';
     const GREATER = '>';
+    const GREATER_OR_EQUAL = '>=';
     const LIKE = 'like';
 
     /*'=', '<', '>', '<=', '>=', '<>', '!=',
