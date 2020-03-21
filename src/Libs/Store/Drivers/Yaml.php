6ce3741d90a65a6db5461f75cb0455bad67e2b39
Vitaliy <theyamshikov@yandex.ru>
2020-03-21 17:43:55 +0600

    fix(yaml)

--- src/Libs/Store/Drivers/Yaml.php
+++ src/Libs/Store/Drivers/Yaml.php
@@ -17,8 +17,8 @@ class Yaml implements DriverInterface
 
     public function encode($data)
     {
-        $content = json_encode($data, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
-        $content = str_replace('    ', "\t", $content);
+        $content = Yaml::dump($data, 10);
+        //$content = str_replace('    ', "\t", $content);
         return $content;
     }
 
